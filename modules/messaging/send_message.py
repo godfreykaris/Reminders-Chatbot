a77f0fd9d194d5ce1a2143090a0e2bfce0fc89d6
godfreykaris Wambugu <godfreykarisw@gmail.com>
2023-09-01 22:11:03 +0000

    Sytem Update

--- modules/messaging/send_message.py
+++ modules/messaging/send_message.py
@@ -1,19 +1,15 @@
 
-from flask import jsonify, request
+import json
 
 from modules.messaging.sms_sender import TwilioSMSSender
 from modules.messaging.whatsapp_sender import TwilioWhatsAppSender
 
 
 class MessageSender:
-    def send_message(self):
-        data = request.get_json()
-        recipient = data.get('recipient')
-        message = data.get('message')
-        message_type = data.get('type')  # 'sms' or 'whatsapp'
-
+    def send_message(self, recipient, message, message_type):
+        
         if not recipient or not message or not message_type:
-            return jsonify({'message': 'Invalid input data'}), 400
+            return json.dumps({'message': 'Invalid input data'}), 400
 
         try:
             if message_type == 'sms':
@@ -23,18 +19,18 @@ class MessageSender:
                 message_sender = TwilioWhatsAppSender("config.json")
                 sent = message_sender.send_whatsapp(recipient, message)
             else:
-                return jsonify({'message': 'Invalid message type'}), 400
+                return json.dumps({'message': 'Invalid message type'}), 400
 
             if sent:
-                return jsonify({'message': f'{message_type.capitalize()} sent successfully'})
+                return json.dumps({'message': f'{message_type.capitalize()} sent successfully'})
             else:
-                #return jsonify({'message': f'Failed to send {message_type}'}), 500
+                #return json.dumps({'message': f'Failed to send {message_type}'}), 500
                 # For testing only
-                return jsonify({'message': sent}), 500
+                return json.dumps({'message': sent}), 500
 
     
         except Exception as e:
-            # return jsonify({'message': f'Failed to send {message_type}', 'error': str(e)}), 500
+            # return json.dumps({'message': f'Failed to send {message_type}', 'error': str(e)}), 500
             
             # For testing only
-            return jsonify({'message': str(e), 'error': str(e)}), 500
+            return json.dumps({'message': str(e), 'error': str(e)}), 500
